1c0f7d7ad6a9594a4fd1d5d12415df10de067f31
annie-qiu26 <30778029+annie-qiu26@users.noreply.github.com>
2017-11-29 14:58:18 -0600

    created grid

--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -1,7 +1,27 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.GridLayout;
 
-public class MemoryGame {
+public class MemoryGame extends JFrame{
+	JPanel p=new JPanel();
+	MemoryCards buttons[]=new MemoryCards[100];
 	
+	public static void main(String args[]){
+		new MemoryGame();
+	}
+	
+	public MemoryGame(){
+		super("MemoryGame");
+		setSize(800,800);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		p.setLayout(new GridLayout(10,10));
+		for(int i=0;i<100;i++){
+			buttons[i]=new MemoryCards();
+			p.add(buttons[i]);
+		}
+		add(p);
+		
+		setVisible(true);
+	}	
 }
